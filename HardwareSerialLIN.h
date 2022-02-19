e867280ea9bde895eae775e43117623678cd097d
Sherzaad <57634578+sherzaad@users.noreply.github.com>
2022-02-19 10:59:46 +0000

    Update HardwareSerialLIN.h

--- HardwareSerialLIN.h
+++ HardwareSerialLIN.h
@@ -1,44 +1,10 @@
-/*
-  HardwareSerial.h - Hardware serial library for Wiring
-  Copyright (c) 2006 Nicholas Zambetti.  All right reserved.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-  Modified 28 September 2010 by Mark Sproul
-  Modified 14 August 2012 by Alarus
-  Modified 3 December 2013 by Matthijs Kooijman
-  //--- Modified 21 December 2017 by Sherzaad
-  list of changes:
-  - 9bit serial + 1 stop bit
-  - 9bit serial + 2 stop bit
-  - 9bit serial + even parity
-  - 9bit serial + odd parity
-  - _rx_buffer and _tx_buffer type change to unsigned int
-  - new variables defined (_rx,_tx) to receive/transmit 9-bit serial
-  - write function argument changed from uint8_t to uint16_t
-  ---//
-*/
-
 #ifndef HardwareSerialLIN_h
 #define HardwareSerialLIN_h
 
 #include <inttypes.h>
 
-#include <LIN_defs.h>
-
 #include "Stream.h"
+#include "LIN_defs.h"
 
 // Define constants and variables for buffering incoming serial data.  We're
 // using a ring buffer (I think), in which head is the index of the location
@@ -52,15 +18,29 @@
 // often work, but occasionally a race condition can occur that makes
 // Serial behave erratically. See https://github.com/arduino/Arduino/issues/2405
 #if !defined(SERIAL_TX_BUFFER_SIZE)
+#if ((RAMEND - RAMSTART) < 1023)
 #define SERIAL_TX_BUFFER_SIZE 16
+#else
+#define SERIAL_TX_BUFFER_SIZE 64
+#endif
 #endif
 #if !defined(SERIAL_RX_BUFFER_SIZE)
+#if ((RAMEND - RAMSTART) < 1023)
 #define SERIAL_RX_BUFFER_SIZE 16
+#else
+#define SERIAL_RX_BUFFER_SIZE 64
 #endif
-
+#endif
+#if (SERIAL_TX_BUFFER_SIZE>256)
+typedef uint16_t tx_buffer_index_t;
+#else
 typedef uint8_t tx_buffer_index_t;
-
+#endif
+#if  (SERIAL_RX_BUFFER_SIZE>256)
+typedef uint16_t rx_buffer_index_t;
+#else
 typedef uint8_t rx_buffer_index_t;
+#endif
 
 // Define config for Serial.begin(baud, config);
 #define SERIAL_5N1 0x00
@@ -99,7 +79,7 @@ class HardwareSerialLIN : public Stream
   protected:
     volatile uint8_t * const _ubrrh;
     volatile uint8_t * const _ubrrl;
-	volatile uint8_t * const _ucsra;
+    volatile uint8_t * const _ucsra;
     volatile uint8_t * const _ucsrb;
     volatile uint8_t * const _ucsrc;
     volatile uint8_t * const _udr;
@@ -110,43 +90,51 @@ class HardwareSerialLIN : public Stream
     volatile rx_buffer_index_t _rx_buffer_tail;
     volatile tx_buffer_index_t _tx_buffer_head;
     volatile tx_buffer_index_t _tx_buffer_tail;
-	
-	union { 
-      uint16_t val; 
-      uint8_t bytes[2];
-    } _rx,_tx;
 
     // Don't put any members after these buffers, since only the first
     // 32 bytes of this struct can be accessed quickly using the ldd
     // instruction.
-	unsigned long _timestamp_buffer[SERIAL_RX_BUFFER_SIZE];
     unsigned int _rx_buffer[SERIAL_RX_BUFFER_SIZE];
     unsigned int _tx_buffer[SERIAL_TX_BUFFER_SIZE];
-	unsigned long timeout_bit;
-	unsigned long baudrate;
+    unsigned long _timestamp_buffer[SERIAL_RX_BUFFER_SIZE];
+    unsigned long timeout_bit;
+    unsigned long baudrate;
 
   public:
     inline HardwareSerialLIN(
       volatile uint8_t *ubrrh, volatile uint8_t *ubrrl,
       volatile uint8_t *ucsra, volatile uint8_t *ucsrb,
       volatile uint8_t *ucsrc, volatile uint8_t *udr);
-    void begin(unsigned long baud, uint8_t config = SERIAL_8N1);
+    void begin(unsigned long baud) {
+      begin(baud, SERIAL_8N1);
+    }
+    void begin(unsigned long, uint8_t);
     void end();
-	void send_break(uint8_t brk_bits);
-	int64_t getTimestamp(void); //to be called before read function if used
-	unsigned long Read_Timeout(unsigned long bittimeout = BIT_TIMEOUT); //Sets/Returns read timeout period in microseconds
+    void send_break(uint8_t brk_bits);
+    uint32_t *getTimestamp(void); //to be called before read function if used
+    unsigned long Read_Timeout(unsigned long bittimeout = BIT_TIMEOUT); //Sets/Returns read timeout period in microseconds
     virtual int available(void);
     virtual int peek(void);
     virtual int read(void);
     virtual int availableForWrite(void);
     virtual void flush(void);
-    virtual size_t write(uint16_t);
-    inline size_t write(uint8_t n) { return write((uint16_t)n); }
-    inline size_t write(int n) { return write((uint16_t)n); }
-    inline size_t write(unsigned long n) { return write((uint16_t)n); }
-    inline size_t write(long n) { return write((uint16_t)n); }
+    virtual size_t write(uint8_t);
+    inline size_t write(unsigned long n) {
+      return write((uint8_t)n);
+    }
+    inline size_t write(long n) {
+      return write((uint8_t)n);
+    }
+    inline size_t write(unsigned int n) {
+      return write((uint8_t)n);
+    }
+    inline size_t write(int n) {
+      return write((uint8_t)n);
+    }
     using Print::write; // pull in write(str) and write(buf, size) from Print
-    operator bool() { return true; }
+    operator bool() {
+      return true;
+    }
 
     // Interrupt handlers - Not intended to be called externally
     inline void _rx_complete_irq(void);
@@ -154,22 +142,22 @@ class HardwareSerialLIN : public Stream
 };
 
 #if defined(UBRRH) || defined(UBRR0H)
-  extern HardwareSerialLIN LIN0;
-  #define HAVE_HWSERIAL0
+extern HardwareSerialLIN LIN;
+#define HAVE_HWSERIAL0
 #endif
 #if defined(UBRR1H)
-  extern HardwareSerialLIN LIN1;
-  #define HAVE_HWSERIAL1
+extern HardwareSerialLIN LIN1;
+#define HAVE_HWSERIAL1
 #endif
 #if defined(UBRR2H)
-  extern HardwareSerialLIN LIN2;
-  #define HAVE_HWSERIAL2
+extern HardwareSerialLIN LIN2;
+#define HAVE_HWSERIAL2
 #endif
 #if defined(UBRR3H)
-  extern HardwareSerialLIN LIN3;
-  #define HAVE_HWSERIAL3
+extern HardwareSerialLIN LIN3;
+#define HAVE_HWSERIAL3
 #endif
 
-extern void serialEventRun(void) __attribute__((weak));
+extern void LINserialEventRun(void) __attribute__((weak));
 
 #endif
