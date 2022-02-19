5a404c4d14266db5fc59800c47d68618302f3127
Sherzaad <57634578+sherzaad@users.noreply.github.com>
2022-02-19 10:57:52 +0000

    Update HardwareSerialLIN3.cpp

--- HardwareSerialLIN3.cpp
+++ HardwareSerialLIN3.cpp
@@ -1,36 +1,12 @@
-/*
-  HardwareLIN3.cpp - Hardware LIN library for Wiring
-  Copyright (c) 2006 Nicholas Zambetti.  All right reserved.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-  Modified 23 November 2006 by David A. Mellis
-  Modified 28 September 2010 by Mark Sproul
-  Modified 14 August 2012 by Alarus
-  Modified 3 December 2013 by Matthijs Kooijman
-*/
-
 #include "Arduino.h"
 #include "HardwareSerialLIN.h"
 #include "HardwareSerialLIN_private.h"
 
-// Each HardwareLIN is defined in its own file, sine the linker pulls
+// Each HardwareSerial is defined in its own file, sine the linker pulls
 // in the entire file when any element inside is used. --gc-sections can
 // additionally cause unused symbols to be dropped, but ISRs have the
 // "used" attribute so are never dropped and they keep the
-// HardwareLIN instance in as well. Putting each instance in its own
+// HardwareSerial instance in as well. Putting each instance in its own
 // file prevents the linker from pulling in any unused instances in the
 // first place.
 
@@ -48,9 +24,9 @@ ISR(USART3_UDRE_vect)
 
 HardwareSerialLIN LIN3(&UBRR3H, &UBRR3L, &UCSR3A, &UCSR3B, &UCSR3C, &UDR3);
 
-// Function that can be weakly referenced by LINEventRun to prevent
+// Function that can be weakly referenced by serialEventRun to prevent
 // pulling in this file if it's not otherwise used.
-bool Serial0_available() {
+bool LINSerial3_available() {
   return LIN3.available();
 }
 
