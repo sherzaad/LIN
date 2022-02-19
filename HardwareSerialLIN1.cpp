2c6187b03b1cff0711986e41450303c93654ef08
Sherzaad <57634578+sherzaad@users.noreply.github.com>
2022-02-19 10:58:41 +0000

    Update HardwareSerialLIN1.cpp

--- HardwareSerialLIN1.cpp
+++ HardwareSerialLIN1.cpp
@@ -1,36 +1,12 @@
-/*
-  HardwareLIN1.cpp - Hardware LIN library for Wiring
-  Copyright (c) 2006 Nicholas Zambetti.  All right reserved.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-  Modified 23 November 2006 by David A. Mellis
-  Modified 28 September 2010 by Mark Sproul
-  Modified 14 August 2012 by Alarus
-  Modified 3 December 2013 by Matthijs Kooijman
-*/
-
 #include "Arduino.h"
 #include "HardwareSerialLIN.h"
 #include "HardwareSerialLIN_private.h"
 
-// Each HardwareLIN is defined in its own file, sine the linker pulls
+// Each HardwareSerial is defined in its own file, sine the linker pulls
 // in the entire file when any element inside is used. --gc-sections can
 // additionally cause unused symbols to be dropped, but ISRs have the
 // "used" attribute so are never dropped and they keep the
-// HardwareLIN instance in as well. Putting each instance in its own
+// HardwareSerial instance in as well. Putting each instance in its own
 // file prevents the linker from pulling in any unused instances in the
 // first place.
 
@@ -41,7 +17,7 @@ ISR(UART1_RX_vect)
 #elif defined(USART1_RX_vect)
 ISR(USART1_RX_vect)
 #else
-#error "Don't know what the Data Register Empty vector is called for LIN1"
+#error "Don't know what the Data Register Empty vector is called for Serial1"
 #endif
 {
   LIN1._rx_complete_irq();
@@ -52,7 +28,7 @@ ISR(UART1_UDRE_vect)
 #elif defined(USART1_UDRE_vect)
 ISR(USART1_UDRE_vect)
 #else
-#error "Don't know what the Data Register Empty vector is called for LIN1"
+#error "Don't know what the Data Register Empty vector is called for Serial1"
 #endif
 {
   LIN1._tx_udr_empty_irq();
@@ -60,9 +36,9 @@ ISR(USART1_UDRE_vect)
 
 HardwareSerialLIN LIN1(&UBRR1H, &UBRR1L, &UCSR1A, &UCSR1B, &UCSR1C, &UDR1);
 
-// Function that can be weakly referenced by LINEventRun to prevent
+// Function that can be weakly referenced by serialEventRun to prevent
 // pulling in this file if it's not otherwise used.
-bool LIN1_available() {
+bool LINSerial1_available() {
   return LIN1.available();
 }
 
