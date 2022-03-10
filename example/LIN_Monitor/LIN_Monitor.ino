6aadc242ef76c25f0069fe1b25a486e77281f933
Sherzaad <57634578+sherzaad@users.noreply.github.com>
2022-03-10 06:29:46 +0000

    Update LIN_Monitor.ino

--- example/LIN_Monitor/LIN_Monitor.ino
+++ example/LIN_Monitor/LIN_Monitor.ino
@@ -1,4 +1,4 @@
-#include <LIN.h>
+#include "LIN.h"
 
 #define LIN_BAUD1 9600
 #define LIN_BAUD2 19200UL
@@ -6,7 +6,7 @@
 void setup() {
   /*IMPORTANT BEFORE START*/
   /*
-   * if you are using ANY other the Hw Serial Ports for other that LIN, 
+   * if you are using ANY of the Hw Serial Ports for other that LIN, 
    * you MUST move the corresponding HardwareSerialLINx.cpp file to the 'OtherThanLIN' folder.
    * Else you may have 'multiple definition' compile error 
    */
@@ -21,28 +21,28 @@ void loop() {
   if (LIN1.available()) {
     uint16_t val = LIN1.read();
 
-    if (LIN_FieldType(val) == BREAKFIELD) {
+    if (LIN.FieldType(val) == BREAKFIELD) {
       Serial.println("");
     }
-    else if (LIN_FieldType(val) == NEW_FRAME) {
+    else if (LIN.FieldType(val) == NEW_FRAME) {
       Serial.println("");
       Serial.print("New Frame: ");
       Serial.print(val & 0xFF, HEX);
       Serial.print(", ");
     }
-    else if (LIN_FieldType(val) == SYNC) {
+    else if (LIN.FieldType(val) == SYNC) {
       Serial.print("Sync: ");
       Serial.print(val & 0xFF, HEX);
       Serial.print(", ");
     }
-    else if (LIN_FieldType(val) == PID) {
+    else if (LIN.FieldType(val) == PID) {
       Serial.print("PID: ");
       Serial.print(val & 0xFF, HEX);
       Serial.print(", ID: ");
       Serial.print((val & 0xFF) & 0x3F, HEX);
       Serial.print(", Data: ");
     }
-    else if (LIN_FieldType(val) == DATA) {
+    else if (LIN.FieldType(val) == DATA) {
       Serial.print(val & 0xFF, HEX); //data/checksum bytes. 
                                      //Last received DATA byte of a given LIN frame would normally be the Checksum Byte
       Serial.print(", ");
