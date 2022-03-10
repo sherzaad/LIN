b8e4fec22388c926a0f5c284887ce519c016f524
Sherzaad <57634578+sherzaad@users.noreply.github.com>
2022-03-10 06:30:13 +0000

    Update LIN_ReadWrite.ino

--- example/LIN_ReadWrite/LIN_ReadWrite.ino
+++ example/LIN_ReadWrite/LIN_ReadWrite.ino
@@ -1,10 +1,4 @@
-#include <LIN.h>
-
-/*
-Example to demonstrate sending and receiving LIN message.
-For the purpose of this example, lin message frame is sent out on LIN1 and read back on LIN2 (loopback)
-Example tested on arduino MEGA
-*/
+#include "LIN.h"
 
 #define LIN_BAUD1 9600
 #define LIN_BAUD2 19200UL
@@ -17,7 +11,7 @@ uint8_t Checksum;
 void setup() {
   /*IMPORTANT BEFORE START*/
   /*
-   * if you are using ANY other the Hw Serial Ports for other that LIN, 
+   * if you are using ANY of the Hw Serial Ports for other that LIN, 
    * you MUST move the corresponding HardwareSerialLINx.cpp file to the 'OtherThanLIN' folder.
    * Else you may have 'multiple definition' compile error 
    */
@@ -26,42 +20,42 @@ void setup() {
   LIN2.begin(LIN_BAUD2);
   Serial.println("READY");
   
-  Checksum = LIN_Checksum_Calc(msg_pid, msg_Data, msg_dlc); //assuming LIN Version Protocol 2.x
+  Checksum = LIN.GetChecksum(msg_pid, msg_Data, msg_dlc); //assuming LIN Version Protocol 2.x
 
 }
 
 void loop() {
-  Serial.println("\nSending LIN FRAME...");
-  Send_LIN_Header_Frame(LIN1, LIN_BAUD2, msg_pid);
-  Send_LIN_Data_Frame(LIN1, msg_Data , msg_dlc, Checksum);
+  Serial.print("\nSending LIN FRAME...");
+  LIN.SendHeaderFrame(LIN1, LIN_BAUD2, msg_pid);
+  LIN.SendDataFrame(LIN1, msg_Data , msg_dlc, Checksum);
   
   delay(1000); //arbitrary delay
   
   while (LIN2.available()) {
     uint16_t val = LIN2.read();
 
-    if (LIN_FieldType(val) == BREAKFIELD) {
+    if (LIN.FieldType(val) == BREAKFIELD) {
       Serial.println("");
     }
-    else if (LIN_FieldType(val) == NEW_FRAME) {
+    else if (LIN.FieldType(val) == NEW_FRAME) {
       Serial.println("");
       Serial.print("New Frame: ");
       Serial.print(val & 0xFF, HEX);
       Serial.print(", ");
     }
-    else if (LIN_FieldType(val) == SYNC) {
+    else if (LIN.FieldType(val) == SYNC) {
       Serial.print("Sync: ");
       Serial.print(val & 0xFF, HEX);
       Serial.print(", ");
     }
-    else if (LIN_FieldType(val) == PID) {
+    else if (LIN.FieldType(val) == PID) {
       Serial.print("PID: ");
       Serial.print(val & 0xFF, HEX);
       Serial.print(", ID: ");
       Serial.print((val & 0xFF) & 0x3F, HEX);
       Serial.print(", Data: ");
     }
-    else if (LIN_FieldType(val) == DATA) {
+    else if (LIN.FieldType(val) == DATA) {
       Serial.print(val & 0xFF, HEX); //data/checksum bytes. 
                                      //Last received DATA byte of a given LIN frame would normally be the Checksum Byte
       Serial.print(", ");
