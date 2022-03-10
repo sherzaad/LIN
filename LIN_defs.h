d29dbf1d97dfd3067daf60eca93d2fef5136c1fe
Sherzaad <57634578+sherzaad@users.noreply.github.com>
2022-03-10 06:24:19 +0000

    Update LIN_defs.h

--- LIN_defs.h
+++ LIN_defs.h
@@ -1,3 +1,10 @@
+/*
+  LIN_defs.h- Library for Arduino LIN shield based on Hardware Serial Library for Arduino
+  ver 1.0 created by Sherzaad Dinah
+  Revision History
+  ver1.0 - Newly created
+*/
+
 #ifndef	LIN_DEFS_H
 #define	LIN_DEFS_H
 /*
@@ -6,7 +13,7 @@
   #define CLR_BIT(x,y) ((x)&=(~(1<<(y)))) //clear bit y in byte x
   #define TGL_BIT(x,y) ((x)^=(1<<(y))) //toggle bit y in byte x
 */
-#define LINFRAME_NOMINAL 124	// 124 = 34 +90 [serial_brk(14) + sync_brk_field(10) + PID(10) = 34, 10*8(max payload bytes) + checksum(10) = 90]
+#define LINFRAME_NOMINAL 124	// 124 = 34 +90 [brk_field(14) + sync_brk_field(10) + PID(10) = 34, 10*8(max payload bytes) + checksum(10) = 90]
 #define LINFRAME_TIMEOUT 2	// Wait this many frames before declaring a read timeout.
 #define READTIMEOUT(x,y) (((x)*(y)*14)/10) //specced addtl 40% space above normal hence 1.4*nominal frame.
 //x = LINFRAME_NOMINAL
@@ -14,7 +21,7 @@
 #define BIT_TIMEOUT READTIMEOUT(LINFRAME_TIMEOUT,LINFRAME_NOMINAL)
 #define BIT_VAL(x,y) (((x)&(1<<(y)))>0? 1:0) //returns value selected bit '0' or '1'
 
-#define BREAK_BITS 13 //number of bits in breakfield. (one less to account to serial start bit)
+#define BREAK_BITS 13 //number of bits in brk_field. (one less to account to serial start bit)
 //#define FRAMEERROR 0x0100
 #define BREAKFIELD 0x0100
 #define SYNC 0x0200
@@ -22,5 +29,7 @@
 #define DATA 0x0400
 #define CHECKSUM 0x0500
 #define NEW_FRAME 0x0600
+
+
 
 #endif
